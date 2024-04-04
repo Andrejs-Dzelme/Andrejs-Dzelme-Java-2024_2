c8785b7d5eeb21617af4ee7335e2d06bba0bb46f
Andrejs Dzelme <andrejs.dzelme@venta.lv>
2024-04-04 16:08:49 +0300

    Update FirstController.java

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -1,8 +1,14 @@
 package lv.venta.controller;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import lv.venta.model.Product;
 
 
 @Controller
@@ -20,5 +26,24 @@ public class FirstController {
 		model.addAttribute("mydata", "Ziņa no Java spring.");
 		return "hello-msg-page";
 	}
+	@GetMapping("/product/test")//localhost:8080/product/test
+	public String getProductTest(Model model) {
+		Product tempProduct = new Product("Abols", "Zaļš", 0.99f, 5);
+		model.addAttribute("mydata",tempProduct );
+		return "product-one-show-page";		
+	}
+	@GetMapping("/product/all")//localhost:8080/product/all
+	public String getProductAll(Model model) {
+		Product tempProduct1 = new Product("Abols", "Zaļš", 0.99f, 5);
+		Product tempProduct2 = new Product("Abols", "Sarkans", 1.99f, 50);
+		Product tempProduct3 = new Product("Abols", "Dzeltens", 3.99f, 500);
+		Product tempProduct4 = new Product("Abols", "Brūns", 9.99f, 23);
+		ArrayList<Product> allProducts = new ArrayList<>(Arrays.asList(tempProduct1, tempProduct2, tempProduct3, tempProduct4));
 
+			model.addAttribute("mydata", allProducts);
+			return "show-product-all-page"; //tiek parādīta show-product-all-page.html lapa
+	}
+
+	
+	
 }
