ed96df504424db67319b4215b0392e74c85b7807
Andrejs Dzelme <andrejs.dzelme@venta.lv>
2024-04-13 12:40:42 +0300

    Update FirstController.java

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -4,16 +4,26 @@ package lv.venta.controller;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lv.venta.model.Product;
-
+import lv.venta.service.ICRUDProductService;
+import lv.venta.service.IFilterProductService;
 
 @Controller
-
 public class FirstController {
+	
+	@Autowired
+	private ICRUDProductService crudService;
+	
+	@Autowired
+	private IFilterProductService filterService;
 	
 	@GetMapping("/hello")//localhost:8080/hello
 	public String getHello() {
@@ -28,22 +38,51 @@ public class FirstController {
 	}
 	@GetMapping("/product/test")//localhost:8080/product/test
 	public String getProductTest(Model model) {
-		Product tempProduct = new Product("Abols", "Zaļš", 0.99f, 5);
-		model.addAttribute("mydata",tempProduct );
-		return "product-one-show-page";		
+		try {
+			model.addAttribute("mydata", crudService.retrieveById(1));
+			return "product-one-show-page";// tiek parādīta product-one-show-page.html lapa
+		} catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
+			return "error-page";// tiek parādīta error-page.html lapa
+		}
+					
 	}
 	@GetMapping("/product/all")//localhost:8080/product/all
 	public String getProductAll(Model model) {
-		Product tempProduct1 = new Product("Abols", "Zaļš", 0.99f, 5);
-		Product tempProduct2 = new Product("Abols", "Sarkans", 1.99f, 50);
-		Product tempProduct3 = new Product("Abols", "Dzeltens", 3.99f, 500);
-		Product tempProduct4 = new Product("Abols", "Brūns", 9.99f, 23);
-		ArrayList<Product> allProducts = new ArrayList<>(Arrays.asList(tempProduct1, tempProduct2, tempProduct3, tempProduct4));
-
-			model.addAttribute("mydata", allProducts);
-			return "show-product-all-page"; //tiek parādīta show-product-all-page.html lapa
+		try {
+			model.addAttribute("mydata", crudService.retrieveAll());
+			return "show-product-all-page";// tiek parādīta show-product-all-page.html lapa
+		}
+		catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
+			return "error-page";// tiek parādīta error-page.html lapa
+		}
 	}
 
+	@GetMapping("/product/one")//localhost:8080/product/one?id=5
+	public String getProductOneId(@RequestParam("id")int id, Model model) {
+		try
+		{
+			model.addAttribute("mydata", crudService.retrieveById(id));
+			return "product-one-show-page";// tiek parādīta product-one-show-page.html lapa
+		}
+		catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
+			return "error-page";// tiek parādīta error-page.html lapa
+		}
+	}
 	
+	@GetMapping("/product/all/{id}") //localhost:8080/product/all/2
+	public String getProductAllId(@PathVariable("id") int id, Model model) {
+		try
+		{
+			model.addAttribute("mydata", crudService.retrieveById(id));
+			return "product-one-show-page";// tiek parādīta product-one-show-page.html lapa
+		}
+		catch (Exception e) {
+			model.addAttribute("errormsg", e.getMessage());
+			return "error-page";// tiek parādīta error-page.html lapa
+		}
+	}	
 	
 }
