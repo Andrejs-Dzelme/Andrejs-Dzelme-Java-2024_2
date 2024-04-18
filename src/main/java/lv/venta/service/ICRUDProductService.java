a5cc793ab51945e341db1163d67ae0246da53e38
Andrejs Dzelme <andrejs.dzelme@venta.lv>
2024-04-18 15:34:09 +0300

    Update ICRUDProductService.java

--- src/main/java/lv/venta/service/ICRUDProductService.java
+++ src/main/java/lv/venta/service/ICRUDProductService.java
@@ -2,13 +2,13 @@ package lv.venta.service;
 
 import java.util.ArrayList;
 import lv.venta.model.Product;
-
 public interface ICRUDProductService {
-	//CRUD
-	public abstract Product create(String title, String description, float price, int quantity) throws Exception;
+	
+	//CRUD - create - retrieve - update - delete
+	public abstract Product create(String title, String description, float price, int quantity) throws Exception;	
 	public abstract ArrayList<Product> retrieveAll() throws Exception;
-	public abstract Product retrieveById(int id) throws Exception;
-	public abstract void updateById(int id, String title, String description, float price, int quantity) throws Exception;
+	public abstract Product retrieveById(int id)throws Exception;	
+	public abstract void updateById(int id, String title, String description, float price, int quantity) throws Exception;	
 	public abstract void deleteById(int id) throws Exception;
-
+	
 }
