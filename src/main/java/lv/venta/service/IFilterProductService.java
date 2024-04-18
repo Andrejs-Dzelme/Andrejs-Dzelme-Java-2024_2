eed11c6030cc3a62df9e71d814b5c7914a3ec174
Andrejs Dzelme <andrejs.dzelme@venta.lv>
2024-04-18 15:34:11 +0300

    Update IFilterProductService.java

--- src/main/java/lv/venta/service/IFilterProductService.java
+++ src/main/java/lv/venta/service/IFilterProductService.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import lv.venta.model.Product;
 
 public interface IFilterProductService {
+	
 	public abstract ArrayList<Product> filterByPriceLessThanThreshold(float threshold) throws Exception;
-	public abstract ArrayList<Product> filterByQuantityLessThanThreshold(float threshold) throws Exception;
+	public abstract ArrayList<Product> filterByQuantityLessThanThreshold(int threshold) throws Exception;
 	public abstract ArrayList<Product> filterByTitleOrDescription(String text) throws Exception;
 	public abstract float calculateProductsTotalValue() throws Exception;
+
 }
