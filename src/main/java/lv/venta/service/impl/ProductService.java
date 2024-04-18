e745a2850728ceaa92f9a0bbabd954a24fe27ae0
Andrejs Dzelme <andrejs.dzelme@venta.lv>
2024-04-18 15:34:14 +0300

    Update ProductService.java

--- src/main/java/lv/venta/service/impl/ProductService.java
+++ src/main/java/lv/venta/service/impl/ProductService.java
@@ -9,105 +9,135 @@ import lv.venta.model.Product;
 import lv.venta.service.ICRUDProductService;
 import lv.venta.service.IFilterProductService;
 
-	@Service
-	public class ProductService implements ICRUDProductService, IFilterProductService{
-		private Product tempProduct1 = new Product("Abols", "Zaļš", 0.99f, 5);
-		private Product tempProduct2 = new Product("Abols", "Sarkans", 1.99f, 50);
-		private Product tempProduct3 = new Product("Abols", "Dzeltens", 3.99f, 500);
-		private Product tempProduct4 = new Product("Abols", "Brūns", 9.99f, 23);
-		ArrayList<Product> allProducts = new ArrayList<>(Arrays.asList(tempProduct1, tempProduct2, tempProduct3, tempProduct4));
-		
-		@Override
-		public ArrayList<Product> filterByPriceLessThanThreshold(float threshold) throws Exception {
-			if(threshold <= 0 || threshold > 10000) throw new Exception("The limit of price overreached.");
-			ArrayList<Product> result = new ArrayList<>();
-			for(Product tempP: allProducts) {
-				if(tempP.getPrice() < threshold) {
-					result.add(tempP);
-				}
+@Service
+public class ProductService implements ICRUDProductService, IFilterProductService {
+
+	private ArrayList<Product> allProducts = new ArrayList<>(Arrays.asList(new Product("Abols", "Sarkans", 0.99f, 5),
+			new Product("Zemene", "Salda", 1.99f, 50), new Product("Burkans", "Oranžš", 0.39f, 500)));
+
+	@Override
+	public Product create(String title, String description, float price, int quantity) throws Exception {
+		// pārbaudam ienākošos parametrus
+		if (title == null || description == null || price < 0 || quantity < 0)
+			throw new Exception("Problems with input params");
+
+		// noskaidrojam, vai jau tāds produkts neeksistē
+		for (Product tempP : allProducts) {
+			if (tempP.getTitle().equals(title) && tempP.getDescription().equals(description)
+					&& tempP.getPrice() == price) {
+				tempP.setQuantity(tempP.getQuantity() + quantity);
+				return tempP;
 			}
-			return result;
 		}
+		// ja tāds produkts neeksistē, tad izveidojam jaunu
+		Product newProduct = new Product(title, description, price, quantity);
+		allProducts.add(newProduct);
+		return newProduct;
 
-		@Override
-		public ArrayList<Product> filterByQuantityLessThanThreshold(float threshold) throws Exception {
-			if(threshold <= 0 || threshold > 100) throw new Exception("The limit of quantity overreached.");
-			ArrayList<Product> result = new ArrayList<>();
-			for(Product tempP: allProducts) {
-				if(tempP.getQuantity() < threshold) {
-					result.add(tempP);
-				}
-			}
-			return result;
-		}
-
-		@Override
-		public ArrayList<Product> filterByTitleOrDescription(String text) throws Exception {
-			if(text == null) throw new Exception("Search text cannot be null.");
-			ArrayList<Product> result = new ArrayList<>();
-			for(Product tempP: allProducts) {
-				if(tempP.getTitle() == text) {
-					result.add(tempP);
-				}
-			}
-			return result;
-		}
-
-		@Override
-		public float calculateProductsTotalValue() throws Exception {
-			if (allProducts.isEmpty())
-				throw new Exception("Product list is empty");
-			
-			
-			float result = 0;
-			for(Product tempP: allProducts) {
-				result += tempP.getPrice()*tempP.getQuantity();
-			}
-			
-			return result;
-		}
-
-		@Override
-		public Product create(String title, String description, float price, int quantity) throws Exception {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		@Override
-		public ArrayList<Product> retrieveAll() throws Exception{
-			if(allProducts.isEmpty()) throw new Exception("Product list is empty.");
-			return allProducts;
-		}
-
-		@Override
-		public Product retrieveById(int id) throws Exception {
-			if(id > 0) {
-				for(Product tempP: allProducts) {
-					if(tempP.getId() == id) {
-						return tempP;
-					}
-				}
-				throw new Exception("ID should be positive.");
-			}
-			return null;
-		}
-
-		@Override
-		public void updateById(int id, String title, String description, float price, int quantity) throws Exception {
-			Product updateProduct = retrieveById(id);
-			if(title != null) updateProduct.setTitle(title);
-			if(description != null) updateProduct.setDescription(description);
-			if(price >= 0 && price <= 10000) updateProduct.setPrice(price);
-			if(quantity >= 0 && quantity <= 100) updateProduct.setQuantity(quantity);
-		}
-
-		@Override
-		public void deleteById(int id) throws Exception {
-			Product deleteProduct = retrieveById(id);
-			allProducts.remove(deleteProduct);		
-		}
-		
-	
 	}
-	
 
+	@Override
+	public ArrayList<Product> retrieveAll() throws Exception {
+		if (allProducts.isEmpty())
+			throw new Exception("Product list is empty");
+
+		return allProducts;
+	}
+
+	@Override
+	public Product retrieveById(int id) throws Exception {
+		if (id > 0) {
+			for (Product tempP : allProducts) {
+				if (tempP.getId() == id) {
+					return tempP;
+				}
+			}
+
+			throw new Exception("Product with " + id + " is not found");
+
+		} else {
+			throw new Exception("Id should be positive");
+		}
+	}
+
+	@Override
+	public void updateById(int id, String title, String description, float price, int quantity) throws Exception {
+		Product updateProduct = retrieveById(id);
+		if(title != null) updateProduct.setTitle(title);
+		if(description !=null) updateProduct.setDescription(description);
+		if(price >= 0 && price <= 10000) updateProduct.setPrice(price);
+		if(quantity >= 0 && quantity <= 100 ) updateProduct.setQuantity(quantity);
+
+	}
+
+	@Override
+	public void deleteById(int id) throws Exception {
+		Product deleteProduct = retrieveById(id);
+		allProducts.remove(deleteProduct);
+
+	}
+
+	@Override
+	public ArrayList<Product> filterByPriceLessThanThreshold(float threshold) throws Exception {
+		if(threshold < 0 || threshold > 10000 ) throw new Exception("The limit of price is wrong");
+		
+		ArrayList<Product> result = new ArrayList<>();
+		
+		for(Product tempP: allProducts) {
+			if(tempP.getPrice() < threshold)
+			{
+				result.add(tempP);
+			}
+		}
+		
+		return result;
+	}
+
+	@Override
+	public ArrayList<Product> filterByQuantityLessThanThreshold(int threshold) throws Exception {
+		if(threshold < 0 || threshold > 100 ) throw new Exception("The limit of quantity is wrong");
+		
+		ArrayList<Product> result = new ArrayList<>();
+		
+		for(Product tempP: allProducts) {
+			if(tempP.getQuantity() < threshold)
+			{
+				result.add(tempP);
+			}
+		}
+		
+		return result;
+	}
+
+	@Override
+	public ArrayList<Product> filterByTitleOrDescription(String text) throws Exception {
+		if(text == null) throw new Exception("Search text can not be null");
+		
+		ArrayList<Product> result = new ArrayList<>();
+		
+		for(Product tempP: allProducts) {
+			if(tempP.getTitle().contains(text) || tempP.getDescription().contains(text))
+			{
+				result.add(tempP);
+			}
+		}
+		
+		return result;
+		
+	}
+
+	@Override
+	public float calculateProductsTotalValue() throws Exception {
+		if (allProducts.isEmpty())
+			throw new Exception("Product list is empty");
+		
+		
+		float result = 0;
+		for(Product tempP: allProducts) {
+			result += tempP.getPrice()*tempP.getQuantity();
+		}
+		
+		return result;
+	}
+
+}
