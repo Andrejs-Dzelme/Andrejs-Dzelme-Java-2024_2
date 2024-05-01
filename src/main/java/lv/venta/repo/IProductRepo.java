c1616051de53675d18a02c52eb17bf3cd2fee741
Andrejs Dzelme <andrejs.dzelme@venta.lv>
2024-05-01 19:18:37 +0300

    Update IProductRepo.java

--- src/main/java/lv/venta/repo/IProductRepo.java
+++ src/main/java/lv/venta/repo/IProductRepo.java
@@ -1,8 +1,19 @@
 package lv.venta.repo;
 
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import lv.venta.model.Product;
 
 public interface IProductRepo extends CrudRepository<Product, Integer>{
+
+	Product findByTitleAndDescriptionAndPrice(String title, String description, float price);
+	ArrayList<Product> findByPriceLessThan(float threshold);
+	ArrayList<Product> findByQuantityLessThan(int threshold);
+	ArrayList<Product> findByTitleOrDescriptionContaining(String text, String text2);
+
+	@Query(nativeQuery = true, value = "SELECT SUM(PRICE * QUANTITY) FROM PRODUCT_TABLE;")
+	float calculateToTotalValueFromDB();
 	
 }
